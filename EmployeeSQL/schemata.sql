7d2ba82c6213816b558f060af54e86a23cb71f8a
Miguel-Ari <120321886+Miguel-Ari@users.noreply.github.com>
2023-03-02 21:48:25 -0500

    Add files via upload

---
+++ EmployeeSQL/schemata.sql
@@ -0,0 +1,51 @@
+-- Create titles table
+CREATE TABLE titles(
+	title_id VARCHAR(5) NOT NULL,
+	title VARCHAR(30) NOT NULL,
+    PRIMARY KEY (title_id)	
+);
+
+-- Create employee table
+CREATE TABLE employees(
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1)NOT NULL,
+	hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create departments table
+CREATE TABLE departments(
+    dept_no VARCHAR(8) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
+-- Create salaries table
+CREATE TABLE salaries(
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager(
+    dept_no VARCHAR(8) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp(
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(8) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
